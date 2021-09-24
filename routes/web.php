27c7a577fd5d312c438233e9018d0564fe5592ce
DavidIberfurgo <david.rodrigo@iberfurgo.com>
2021-09-24 17:32:35 +0200

    prueba tonta

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('index');
 });
 
+
+
 Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth', 'verified']);
