a5e0b446dcae8fd8a088791da10429612a9a235c
Sergio Abril <sergioabril@MacBook-Air-de-Sergio.local>
2022-01-04 13:56:06 +0100

    arreglo fallo

--- resources/views/livewire/car-listing-component.blade.php
+++ resources/views/livewire/car-listing-component.blade.php
@@ -482,35 +482,35 @@
                             <div class="car-item gray-bg text-center">
                                 <div class="car-image">
                                     @if (isset($vehiculo->fotos))
-                                            <div id="carouselExampleControls" class="carousel slide"
-                                                data-ride="carousel">
-                                                <div class="carousel-inner">
-                                                    @foreach ($vehiculo->fotos as $foto)
-                                                        @if ($foto->portada == 1)
-                                                            <div class="carousel-item active">
-                                                                <img class="d-block w-100" src="{{ $foto->foto }}"
-                                                                    alt="First slide">
-                                                            </div>
-                                                        @else
-                                                            <div class="carousel-item">
-                                                                <img class="d-block w-100" src="{{ $foto->foto }}"
-                                                                    alt="First slide">
-                                                            </div>
-                                                        @endif
-                                                    @endforeach
-                                                </div>
-                                                <a class="carousel-control-prev" href="#carouselExampleControls"
-                                                    role="button" data-slide="prev">
-                                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                                    <span class="sr-only">Previous</span>
-                                                </a>
-                                                <a class="carousel-control-next" href="#carouselExampleControls"
-                                                    role="button" data-slide="next">
-                                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                                    <span class="sr-only">Next</span>
-                                                </a>
+                                        <div id="carouselExampleControls" class="carousel slide"
+                                            data-ride="carousel">
+                                            <div class="carousel-inner">
+                                                @foreach ($vehiculo->fotos as $foto)
+                                                    @if ($foto->portada == 1)
+                                                        <div class="carousel-item active">
+                                                            <img class="d-block w-100" src="{{ $foto->foto }}"
+                                                                alt="First slide">
+                                                        </div>
+                                                    @else
+                                                        <div class="carousel-item">
+                                                            <img class="d-block w-100" src="{{ $foto->foto }}"
+                                                                alt="First slide">
+                                                        </div>
+                                                    @endif
+                                                @endforeach
                                             </div>
-                                        @endif
+                                            <a class="carousel-control-prev" href="#carouselExampleControls"
+                                                role="button" data-slide="prev">
+                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span class="sr-only">Previous</span>
+                                            </a>
+                                            <a class="carousel-control-next" href="#carouselExampleControls"
+                                                role="button" data-slide="next">
+                                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <span class="sr-only">Next</span>
+                                            </a>
+                                        </div>
+                                    @endif
 
                                 </div>
 
@@ -544,9 +544,9 @@
                                 </div>
                             </div>
                         </div>
-                        @endforeach
-
+                    </div>
                 </div>
+                @endforeach
                 {{-- @foreach ($vehiculos[1]->vehiculos as $vehiculo)
                     <div class="car-grid">
                         <div class="row">
